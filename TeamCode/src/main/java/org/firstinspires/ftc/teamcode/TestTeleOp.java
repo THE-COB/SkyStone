019a6f9c5e63e89f25ebbd241485f59508998a25
avestech <avesablaze@gmail.com>
2019-09-26 15:42:59 -0400

    added TestColorSensor program, it worked

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TestTeleOp.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TestTeleOp.java
@@ -2,8 +2,12 @@ package org.firstinspires.ftc.teamcode;
 
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.ColorSensor;
 import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DistanceSensor;
 import com.qualcomm.robotcore.hardware.Servo;
+
+import org.firstinspires.ftc.robotcore.external.navigation.DistanceUnit;
 
 @TeleOp(name = "test", group = "a" )
 public class TestTeleOp extends LinearOpMode {
@@ -11,20 +15,34 @@ public class TestTeleOp extends LinearOpMode {
     private DcMotor motor0 = null;
     private Servo servo0 = null;
 
+    private ColorSensor color = null;
+    private DistanceSensor distance = null;
+
     @Override
     public void runOpMode() {
         telemetry.addData("test", "we started maybe");
         telemetry.update();
-
+/*
         motor0 = hardwareMap.get(DcMotor.class, "motor0");
-        servo0 = hardwareMap.get(Servo.class, "servo0");
+        servo0 = hardwareMap.get(Servo.class, "servo0");*/
 
-        motor0.setDirection(DcMotor.Direction.FORWARD);
-        servo0.setPosition(0);
+        color = hardwareMap.get(ColorSensor.class, "color-range");
+        distance = hardwareMap.get(DistanceSensor.class, "color-range");
+
+
+/*        motor0.setDirection(DcMotor.Direction.FORWARD);
+        servo0.setPosition(0);*/
 
         waitForStart();
         while (opModeIsActive()) {
-            motor0.setPower(gamepad1.left_stick_y);
+            telemetry.addData("Red",color.red());
+            telemetry.addData("Green",color.green());
+            telemetry.addData("Blue", color.blue());
+            telemetry.addData("Alpha", color.alpha());
+
+            telemetry.addData("Distance(cm)", distance.getDistance(DistanceUnit.CM));
+
+/*            motor0.setPower(gamepad1.left_stick_y);
 
             if(gamepad1.a){
                 servo0.setPosition(0.5);
@@ -36,7 +54,7 @@ public class TestTeleOp extends LinearOpMode {
                 servo0.setPosition(0);
             }
             telemetry.addData("encoderVal", motor0.getCurrentPosition());
-            telemetry.addData("servoVal", servo0.getPosition());
+            telemetry.addData("servoVal", servo0.getPosition());*/
             telemetry.update();
         }
     }
